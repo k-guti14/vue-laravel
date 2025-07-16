1b2acc46159d6bdc6637799874ce46377bb75f8c
Kento Nakamura <naka_kento_1207@icloud.com>
2025-07-16 23:35:01 +0900

    [Modify]SPA環境セットアップ

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,16 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'localhost', // DockerやVagrantならIPにする
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
 });
