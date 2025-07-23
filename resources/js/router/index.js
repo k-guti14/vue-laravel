783786c700f1dde5d8ebe6d59fc1d6bb3bf96864
Kento Nakamura <naka_kento_1207@icloud.com>
2025-07-23 23:55:54 +0900

    [Modify]ルーティングを変更

---
+++ resources/js/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from '../App.vue';
+
+const routes = [
+    { path: '/', component: Home },
+];
+
+export default createRouter({
+    history: createWebHistory(),
+    routes
+});
