36b6ebb6b737f354c7cc5cbfe5a7a85bbff73246
Kento Nakamura <naka_kento_1207@icloud.com>
2025-07-29 23:59:28 +0900

    [Modify]ルーティングを変更

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -4,7 +4,11 @@ import Home from '../pages/Home.vue'
 import About from '../pages/About.vue'
 
 const routes = [
-    { path: '/', name: 'home', component: Home },
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
     { path: '/about', name: 'about', component: About }
 ]
 
